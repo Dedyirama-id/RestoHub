a2ca5a1b75743c1bc5fae0e960992f63d3e6c6e6
Dedy Irama <dedyirama.ars@gmail.com>
2024-03-18 18:42:32 +0700

    changed sw to workbox

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images',
-  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
