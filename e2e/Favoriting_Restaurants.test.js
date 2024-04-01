f54ae6a064f1bd4429d9df8746419d2ded3b9075
Dedy Irama <dedyirama.ars@gmail.com>
2024-04-01 13:22:55 +0700

    Added e2e test: favoriting then unfavoriting restaurant

--- e2e/Favoriting_Restaurants.test.js
+++ e2e/Favoriting_Restaurants.test.js
@@ -7,43 +7,43 @@ Before(({ I }) => {
   I.amOnPage('/#/favorite');
 });
 
-xScenario('Showing empty favorited restaurants', ({ I }) => {
+Scenario('Showing empty favorited restaurants', ({ I }) => {
   I.seeElement('#query');
   I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
 });
 
-xScenario('Favoriting one restaurant', async ({ I }) => {
+Scenario('Favoriting one restaurant', async ({ I }) => {
   I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
 
   I.amOnPage('/');
 
   I.seeElement('.restaurant-item');
 
-  const firstRestaurant = locate('shadow=.restaurant-item__name');
+  const firstRestaurant = locate('.restaurant-item__name');
   const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
   I.click('.restaurant-item');
-  I.seeElement('add-to-favorite-button');
-  I.click('add-to-favorite-button');
+  I.seeElement('[aria-label="add to favorite"]');
+  I.click('[aria-label="add to favorite"]');
 
   I.amOnPage('/#/favorite');
   I.seeElement('.restaurant-item');
 
-  const favoritedRestaurantName = await I.grabTextFrom('shadow=.restaurant-item__name');
+  const favoritedRestaurantName = await I.grabTextFrom('.restaurant-item__name');
   assert.strictEqual(firstRestaurantName, favoritedRestaurantName);
 });
 
-xScenario('searching restaurants', async ({ I }) => {
+Scenario('searching restaurants', async ({ I }) => {
   I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
 
   I.amOnPage('/');
   I.waitForElement('.restaurant-item');
-  I.seeElement('shadow=.restaurant-item');
+  I.seeElement('.restaurant-item');
 
   const names = [];
   for (let i = 1; i <= 3; i++) {
     I.click(locate('.restaurant-item').at(i));
-    I.seeElement('add-to-favorite-button');
-    I.click('add-to-favorite-button');
+    I.seeElement('[aria-label="add to favorite"]');
+    I.click('[aria-label="add to favorite"]');
     names.push(await I.grabTextFrom('.title'));
     I.amOnPage('/');
   }
@@ -68,4 +68,35 @@ xScenario('searching restaurants', async ({ I }) => {
   for (let i = 0; i < matchingRestaurant.length; i++) {
     assert.strictEqual(matchingRestaurant[i], visibleNames[i]);
   }
+});
+
+Scenario('Favoriting one restaurant then remove it from favorite', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant-item');
+
+  const firstRestaurant = locate('.restaurant-item__name');
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click('.restaurant-item');
+  I.seeElement('[aria-label="add to favorite"]');
+  I.click('[aria-label="add to favorite"]');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
+
+  const totalFavoritedRestaurant = await I.grabNumberOfVisibleElements('.restaurant-item');
+  assert.strictEqual(totalFavoritedRestaurant, 1);
+
+  const favoritedRestaurantName = await I.grabTextFrom('.restaurant-item__name');
+  assert.strictEqual(firstRestaurantName, favoritedRestaurantName);
+
+  I.click('.restaurant-item');
+  I.seeElement('[aria-label="remove from favorite"]');
+  I.click('[aria-label="remove from favorite"]');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#query');
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
 });
