1f01a5452b9df721344e9dd0e687bff643bc481b
Dedy Irama <dedyirama.ars@gmail.com>
2024-04-01 10:42:22 +0700

    Firebase initialization

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,13 @@ import './components/card-hover';
 import './components/review-card';
 import './components/add-to-favorite-btn';
 import './components/loader-status';
+import { initializeApp } from 'firebase/app';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import firebaseConfig from './routes/firebase-config';
+
+// eslint-disable-next-line no-unused-vars
+const fbApp = initializeApp(firebaseConfig);
 
 const app = new App({
   button: document.querySelector('#menu-btn'),
